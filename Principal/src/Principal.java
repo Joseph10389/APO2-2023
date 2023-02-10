6f362afcd188a562b02c55f65f8d539bf7d3b17c
Joseph10389 <fabio.cuero00@usc.edu.co>
2023-02-10 16:32:38 -0500

    clase 1

--- Principal/src/Principal.java
+++ Principal/src/Principal.java
@@ -7,13 +7,20 @@ public class Principal {
 	
 	//metodo principal
 	public static void main(String[] args) {
+		
+		System.out.println(contarPalabra()); 
+	}
+	
+	public static String contarPalabra() {
+		
 		System.out.println("ingrese la palabra");
 		//clase que permite capturar datos por consola
 		Scanner sc = new Scanner(System.in);
 		
 		String prueba = sc.next();
-		System.out.println("la cantidad de letras que tiene la palabra es : " +prueba.length()+" caracteres");
-    
+		
+		return "la cantidad de letras que tiene la palabra es : " +prueba.length()+" caracteres";
+		
     
 	}
 }
